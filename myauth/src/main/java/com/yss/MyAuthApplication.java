9c63ac4a8ca1c2eef8be3948c9caad4c392d0451
shuoshi.yan <3050313709@qq.com>
2020-08-07 14:23:16 +0800

    update config project

--- myauth/src/main/java/com/yss/MyAuthApplication.java
+++ myauth/src/main/java/com/yss/MyAuthApplication.java
@@ -2,9 +2,10 @@ package com.yss;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-
+@EnableEurekaClient
 public class MyAuthApplication {
 
   public static void main(String[] args) {
