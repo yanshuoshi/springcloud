98ec0ae5d6dc783d1bc4f9fe1c55fb5fd1e4db9a
shuoshi.yan <3050313709@qq.com>
2020-08-10 16:16:05 +0800

    update cofig

--- myauth/src/main/java/com/yss/MyAuthApplication.java
+++ myauth/src/main/java/com/yss/MyAuthApplication.java
@@ -2,10 +2,16 @@ package com.yss;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableFeignClients
+@EnableHystrixDashboard
+@EnableCircuitBreaker
 public class MyAuthApplication {
 
   public static void main(String[] args) {
